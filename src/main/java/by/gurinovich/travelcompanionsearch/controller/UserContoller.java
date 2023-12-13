340e7d6b8961a4fd615ccf43d92b071146c831ac
Roman Gurinovich <gurinovi4rm@gmail.com>
2023-12-13 22:42:37 +0300

    auth completed

--- src/main/java/by/gurinovich/travelcompanionsearch/controller/UserContoller.java
+++ src/main/java/by/gurinovich/travelcompanionsearch/controller/UserContoller.java
@@ -1,14 +1,18 @@
 package by.gurinovich.travelcompanionsearch.controller;
 
 
+import by.gurinovich.travelcompanionsearch.dto.UserDTO;
 import by.gurinovich.travelcompanionsearch.service.UserService;
 import by.gurinovich.travelcompanionsearch.util.mapper.impl.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -20,8 +24,13 @@ public class UserContoller {
 
     @GetMapping
     public ResponseEntity<Object> getAll(){
-        return new ResponseEntity<>(
-                userMapper.toDTOs(userService.getAll()),
-                HttpStatus.OK);
+        return ResponseEntity.ok()
+                .body(userMapper.toDTOs(userService.getAll()));
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable("user_id") UUID id){
+        return ResponseEntity.ok()
+                .body(userMapper.toDTO(userService.getById(id)));
     }
 }
